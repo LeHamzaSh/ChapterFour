cbe6de6a09bddd6baa7146fe9fb392e6801910c6
LeHamzaSh <hrsheikh05@gmail.com>
2020-12-23 13:45:26 +0400

    updated index.html and index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var firstName = 'Sandu';
+var jobTitle = "Petroleum Engineer";
+var CurrentAddress = "Al Ain, United Arab Emirates";
+
+console.log(firstName);
+console.log(jobTitle);
+console.log(CurrentAddress);
